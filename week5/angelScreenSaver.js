1643ed65260495509a05ae0aeb8f07b9068405b1
Andrew Bentzen <bentzen.an@gmail.com>
2023-02-10 07:29:04 -0500

    old windows screen saver: box sucessfully moves to the right and out of the view.

--- week5/angelScreenSaver.js
+++ week5/angelScreenSaver.js
@@ -1,90 +1,104 @@
 "use strict";
 
-//
-// Display a Mandelbrot set
-//
-
-var canvas;
 var gl;
 
-/* default data*/
+var delta = 0.0;
+var deltaLoc;
+var theta = 0.0;
+var thetaLoc;
 
-/* N x M array to be generated */
+var delay = 100;
+var direction = true;
 
-var scale = 0.125;
-var cx = -2.0; /* center of window in complex plane */
-var cy = -1.0;
-var max = 100; /* number of interations per point */
-
-var n = 1024;
-var m = 1024;
-
-var program;
-
-//----------------------------------------------------------------------------
-
-onload = function init() {
-  canvas = document.getElementById("gl-canvas");
+window.onload = function init() {
+  var canvas = document.getElementById("gl-canvas");
 
   gl = WebGLUtils.setupWebGL(canvas);
   if (!gl) {
     alert("WebGL isn't available");
   }
 
+  //
+  //  Configure WebGL
+  //
   gl.viewport(0, 0, canvas.width, canvas.height);
-
-  // Create and initialize a buffer object
-
-  var points = [
-    vec4(0.0, 0.0, 0.0, 1.0),
-    vec4(0.0, 1.0, 0.0, 1.0),
-    vec4(1.0, 1.0, 0.0, 1.0),
-    vec4(1.0, 1.0, 0.0, 1.0),
-    vec4(1.0, 0.0, 0.0, 1.0),
-    vec4(0.0, 0.0, 0.0, 1.0),
-  ];
-
-  // Load shaders and use the resulting shader program
-
-  program = initShaders(gl, "vertex-shader", "fragment-shader");
-  gl.useProgram(program);
-
-  // set up vertex arrays
-  var buffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  var vPosition = gl.getAttribLocation(program, "vPosition");
-
-  gl.enableVertexAttribArray(vPosition);
-  gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
-  gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
-
-  gl.uniform1f(gl.getUniformLocation(program, "scale"), scale);
-  gl.uniform1f(gl.getUniformLocation(program, "cx"), cx);
-  gl.uniform1f(gl.getUniformLocation(program, "cy"), cy);
-
-  document.getElementById("Center X").onchange = function (event) {
-    cx = event.target.value;
-    gl.uniform1f(gl.getUniformLocation(program, "cx"), cx);
-  };
-  document.getElementById("Center Y").onchange = function (event) {
-    cy = event.target.value;
-    gl.uniform1f(gl.getUniformLocation(program, "cy"), cy);
-  };
-  document.getElementById("Size").onchange = function (event) {
-    scale = 1.0 / event.target.value;
-    gl.uniform1f(gl.getUniformLocation(program, "scale"), scale);
-  };
-
   gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
-  gl.viewport(0, 0, canvas.width, canvas.height);
+  //  Load shaders and initialize attribute buffers
+
+  var program = initShaders(gl, "vertex-shader", "fragment-shader");
+  gl.useProgram(program);
+
+  var vertices = [vec2(1, 1), vec2(-1, 1), vec2(1, -1), vec2(-1, -1)];
+  vertices = vertices.map((vector) => scale(0.1, vector));
+
+  // Load the data into the GPU
+
+  var vBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+
+  // Associate out shader variables with our data buffer
+
+  var vPosition = gl.getAttribLocation(program, "vPosition");
+
+  gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vPosition);
+
+  deltaLoc = gl.getUniformLocation(program, "delta");
+  thetaLoc = gl.getUniformLocation(program, "theta");
+
+  // Initialize event handlers
+  document.getElementById("Direction").onclick = function () {
+    direction = !direction;
+  };
+
+  document.getElementById("Controls").onclick = function (event) {
+    switch (event.target.index) {
+      case 0:
+        direction = !direction;
+        break;
+      case 1:
+        delay /= 2.0;
+        break;
+      case 2:
+        delay *= 2.0;
+        break;
+    }
+  };
+
+  window.onkeydown = function (event) {
+    var key = String.fromCharCode(event.keyCode);
+    switch (key) {
+      case "1":
+        direction = !direction;
+        break;
+
+      case "2":
+        delay /= 2.0;
+        break;
+
+      case "3":
+        delay *= 2.0;
+        break;
+    }
+  };
   render();
 };
 
-//----------------------------------------------------------------------------
-
-var render = function () {
+function render() {
   gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.drawArrays(gl.TRIANGLES, 0, 6);
-  requestAnimFrame(render);
-};
+
+  // theta += direction ? 0.01 : -0.01;
+  theta = 0;
+  delta += direction ? 0.01 : -0.01;
+
+  gl.uniform1f(deltaLoc, delta);
+  gl.uniform1f(thetaLoc, theta);
+
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+  setTimeout(function () {
+    requestAnimFrame(render);
+  }, delay);
+}
